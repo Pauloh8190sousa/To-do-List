b1d5588cf330e52da13b7a2d0be17810e845d8ef
Pauloh8190sousa <paulohenrique8190@gmail.com>
2022-01-18 17:48:45 -0300

    Listagem de tarefas e estilizaçãodo header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import * as C from "./App.styles";
-
+import { Item } from "./types/item";
 export default function App() {
-  return <C.Container></C.Container>;
+  const [list, setList] = useState<Item[]>([
+    { id: 10, name: "teste", done: true },
+  ]);
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>To Do List</C.Header>
+      </C.Area>
+    </C.Container>
+  );
 }
